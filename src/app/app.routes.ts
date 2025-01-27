1f28761e24b21efd7545defaea8def2e751ad92a
Staci Southerland <stacisoutherland24@gmail.com>
2025-01-26 20:33:35 -0600

    Basic skeleton for site. Add angular routing and pages needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { WarehouseComponent } from './pages/warehouse/warehouse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'about', component: AboutComponent }, 
+  { path: 'registration', component: RegistrationComponent }, 
+  { path: 'warehouse', component: WarehouseComponent }, 
+];
---
+++ src/app/pages/warehouse/warehouse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse',
+  standalone: true,
+  imports: [],
+  templateUrl: './warehouse.component.html',
+  styleUrl: './warehouse.component.css'
+})
+export class WarehouseComponent {
+
+}
