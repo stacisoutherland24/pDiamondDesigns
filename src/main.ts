4628f914fe591d8f174ed31c17688f7d7d94c408
Staci Southerland <stacisoutherland24@gmail.com>
2025-07-08 16:46:33 -0500

    feat: added persistence for cart for page refreshes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { provideRouter } from '@angular/router';
 import { provideNgxStripe } from 'ngx-stripe';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { CartEffects } from './app/store/cart/cart.effects'; 
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { isDevMode } from '@angular/core';
 import { cartReducer } from './app/store/cart/cart.reducer';
@@ -18,7 +19,7 @@ bootstrapApplication(AppComponent, {
       'pk_test_51MKUZrFIGbwCUIy0OnSwPPQaOJbyal7tDwipac7YcQupNwbu9Kd3u9RpSWx8Qb9fWWKWrp4vfG6NyptY7ayFbK3a00OghXy9gO'
     ),
     provideStore({ cart: cartReducer }),
-    provideEffects(),
+    provideEffects([CartEffects]),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 }).catch((err) => console.error(err));
